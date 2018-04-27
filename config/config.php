c66b74afe1df0d9fa3e52ca8526b6404e7b0992c
liuhui <icandebug@foxmail.com>
2018-04-27 22:09:11 +0800

    新增配置

---
+++ config/config.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: liuhui
+ * Date: 18-4-27
+ * Time: 下午10:07
+ */
+return [
+];
---
+++ config/db.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: liuhui
+ * Date: 18-4-27
+ * Time: 下午10:08
+ */
